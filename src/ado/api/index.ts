c3cb24409218e94a0fa1d7736303a2da2bffc397
irisxzhou <irisxzhou@protonmail.com>
2023-02-14 14:13:44 -0800

    Query ADO API for item details in a sprint

--- src/ado/api/index.ts
+++ src/ado/api/index.ts
@@ -1,3 +1,51 @@
-export function GetIterations(onlyCurrentIteration = false) {
-    
+export async function GetIterations(
+  organization: string,
+  project: string,
+  team: string,
+  onlyCurrentIteration = false) {
+    let url = `https://dev.azure.com/${organization}/${project}/${team}/_apis/work/teamsettings/iterations`;
+    if (onlyCurrentIteration) { url += "/?$timeframe=current"};
+
+    const json = await fetch(url).then((response) => response.json());
+    return json;
+}
+
+export async function GetItemsFromIteration(
+  organization: string,
+  project: string,
+  team: string,
+  iterationId: string) {
+    const url = `https://dev.azure.com/${organization}/${project}/${team}/_apis/work/teamsettings/iterations/${iterationId}/workitems?api-version=7.0`
+
+    const json = await fetch(url).then((response) => response.json());
+    return json;
+}
+
+export async function GetBatchItemDetails(
+  organization: string,
+  project: string,
+  itemIds: string[]) {
+    const obj = {
+      ids: itemIds,
+      fields: [
+        "System.Id",
+        "System.Title",
+        "System.WorkItemType",
+        "Microsoft.VSTS.Scheduling.RemainingWork",
+        "System.AreaPath",
+        "System.IterationPath",
+        "System.State",
+        "System.Reason",
+        "System.AssignedTo",
+        "Microsoft.VSTS.Scheduling.OriginalEstimate",
+        "OSG.RemainingDevDays"
+      ]
+    }
+    const url = `https://dev.azure.com/${organization}/${project}/_apis/wit/workitemsbatch?api-version=7.1-preview.1`;
+    const json = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(obj),
+      headers: { 'Content-Type': 'application/json'}
+    }).then((response) => response.json());
+    return json;
 }
--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,5 @@
+import { GetBatchItemDetails, GetItemsFromIteration, GetIterations } from "./ado/api";
+
 var adoxChanged = true;
 
 chrome.runtime.onMessage.addListener(
@@ -13,3 +15,30 @@ chrome.runtime.onMessage.addListener(
     return true;
   }
 );
+
+
+chrome.alarms.create(
+  {
+    periodInMinutes: 0.5 // for testing purposes only
+    //periodInMinutes: 360 // once every 6 h
+  }
+)
+
+chrome.alarms.onAlarm.addListener(
+  async function(alarm) {
+    //TODO: read these from the extension popup
+    const organization = "";
+    const project = "";
+    const team = ""
+
+    const iterationsJson = await GetIterations(organization, project, team, true);
+    const currentIterationId = iterationsJson.value[0].id;
+
+    const itemsJson = await GetItemsFromIteration(organization, project, team, currentIterationId);
+    const itemIdsList = itemsJson.workItemRelations.map((item: any) => item.target.id);
+
+    const itemsDetailsJson = await GetBatchItemDetails(organization, project, itemIdsList);
+    const itemDetails = itemsDetailsJson.value;
+    console.log(itemDetails);
+  }
+)
