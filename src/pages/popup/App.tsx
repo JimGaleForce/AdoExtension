6e4c4eb7f15b79e1082bedd078e853e9fdfb1d0f
Alex Casasola <alex@casasola.dev>
2023-02-15 09:45:51 -0800

    Updated popup with expected fields

--- src/pages/popup/App.tsx
+++ src/pages/popup/App.tsx
@@ -1,38 +1,13 @@
 import React from 'react'
 import Highlight from './highlight';
 
-const tabs = [
-  { name: 'ADO Highlighter', href: '#', current: true },
-  { name: 'Settings', href: '#', current: false },
-];
-
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ')
-}
-
 const App = (): JSX.Element => {
   return (
-    <div>
-      <div className="border-b border-gray-200 pl-4 pr-4">
-        <nav className="-mb-px flex space-x-8" aria-label="Tabs">
-          {tabs.map((tab) => (
-            <a
-              key={tab.name}
-              href={tab.href}
-              className={classNames(
-                tab.current
-                  ? 'border-blue-500 text-blue-600'
-                  : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
-                'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm'
-              )}
-              aria-current={tab.current ? 'page' : undefined}
-            >
-              {tab.name}
-            </a>
-          ))}
-        </nav>
+    <div className='min-h-full'>
+      <div className="border-b bg-slate-800 border-slate-600 pb-2">
+        <h3 className="pl-2 pt-2 text-lg font-medium leading-6 text-white">ADO Powertools</h3>
       </div>
-      <div className='my-2'>
+      <div className='p-2 bg-slate-100 min-h-full'>
         <Highlight/>
       </div>
     </div>
--- src/pages/popup/highlight.tsx
+++ src/pages/popup/highlight.tsx
@@ -3,121 +3,258 @@ import React, { useEffect, useState } from 'react';
 type AdoConfigData = {
     queryId: string
     colors: string[]
-  }
-  
-  const baseConfig: AdoConfigData = {
-    queryId: "",
+    email: string
+    organization: string
+    project: string
+    team: string
+}
+
+const baseConfig: AdoConfigData = {
+    queryId:"",
     colors: [
-      '#ccFFcc',
-      '#FFFFcc',
-      '#FFcccc',
-      '#ccccFF',
-      '#ccFFFF'
-    ]
-  }
+        '#ccFFcc',
+        '#FFFFcc',
+        '#FFcccc',
+        '#ccccFF',
+        '#ccFFFF'
+    ],
+    email: "",
+    organization: "microsoft",
+    project: "Edge",
+    team: ""
+}
+
 const Highlight = (): JSX.Element => {
     const [adoxData, setAdoxData] = useState<AdoConfigData>(baseConfig);
     const [loaded, setLoaded] = useState(false);
     const [error, setError] = useState(null);
-  
+
     const saveConfig = async () => {
-      await chrome.storage.sync.set({ adoxData });
-      console.log("Saved config");
+        await chrome.storage.sync.set({ adoxData });
+        console.log("Saved config");
     }
-  
+
     const resetConfig = async () => {
-      setAdoxData(baseConfig);
-      console.log("Config reset");
+        setAdoxData(baseConfig);
+        console.log("Config reset");
     }
 
     const onColorChanged = (index: number, color: string) => {
-        var newData = {...adoxData};
+        var newData = { ...adoxData };
         newData.colors[index] = color;
         setAdoxData(newData);
     }
-  
+
     const onQueryChanged = (queryId: string) => {
-        var newData = {...adoxData};
+        var newData = { ...adoxData };
         newData.queryId = queryId;
         setAdoxData(newData);
     }
 
+    const onEmailChanged = (email: string) => {
+        var newData = { ...adoxData };
+        newData.email = email;
+        setAdoxData(newData);
+    }
+
+    const onOrganizationChanged = (organization: string) => {
+        var newData = { ...adoxData };
+        newData.organization = organization;
+        setAdoxData(newData);
+    }
+
+    const onProjectChanged = (project: string) => {
+        var newData = { ...adoxData };
+        newData.project = project;
+        setAdoxData(newData);
+    }
+
+    const onTeamChanged = (team: string) => {
+        var newData = { ...adoxData };
+        newData.team = team;
+        setAdoxData(newData);
+    }
+
+
     // Load colors from storage
     useEffect(() => {
-      chrome.storage.sync.get(['adoxData'])
-      .then(data => {
-        setAdoxData({...baseConfig, ...data.adoxData});
-        setLoaded(true);
-      })
-      .catch(error => {
-        setError(error);
-      })
+        chrome.storage.sync.get(['adoxData'])
+            .then(data => {
+                setAdoxData({ ...baseConfig, ...data.adoxData });
+                setLoaded(true);
+            })
+            .catch(error => {
+                setError(error);
+            })
     }, [])
-  
+
     if (error) {
-      return (
-        <div>
-          Error loading data
-        </div>
-      )
-    }
-  
-    if (!loaded) {
-      return (
-        <div>
-          Loading...
-        </div>
-      )
-    }
-  
-  return (
-    <div>
-        <div>Line Colors</div>
-        <div>
-            <div id="adox-colors">
-                <input
-                    type="color"
-                    onChange={(e) => onColorChanged(0, e.target.value)}
-                    value={adoxData.colors[0]}
-                    className='w-16px h-20px border-none p-0'
-                />
-                <input
-                    type="color"
-                    onChange={(e) => onColorChanged(1, e.target.value)}
-                    value={adoxData.colors[1]}
-                    className='w-16px h-20px border-none p-0'
-                />
-                <input
-                    type="color"
-                    onChange={(e) => onColorChanged(2, e.target.value)}
-                    value={adoxData.colors[2]}
-                    className='w-16px h-20px border-none p-0'
-                />
-                <input
-                    type="color"
-                    onChange={(e) => onColorChanged(3, e.target.value)}
-                    value={adoxData.colors[3]}
-                    className='w-16px h-20px border-none p-0'
-                />
-                <input
-                    type="color"
-                    onChange={(e) => onColorChanged(4, e.target.value)}
-                    value={adoxData.colors[4]}
-                    className='w-16px h-20px border-none p-0'
-                />
-            </div>
-            <br/>
-            <div>Query ID</div>
+        return (
             <div>
-                <input className='w-100' type="text" value={adoxData.queryId} onChange={(e) => onQueryChanged(e.target.value)} />
+                Error loading data
             </div>
-        </div>
-        <div className='my-2'>
-            <button className="adox-btn button-8" role="button" id="adox-save" onClick={() => saveConfig().then(() =>  window.close())}>Save</button>
-            <button className="adox-btn button-8" role="button" id="adox-reset" onClick={() => resetConfig()}>Reset</button>
-        </div>
-    </div>
-  );
+        )
+    }
+
+    if (!loaded) {
+        return (
+            <div>
+                Loading...
+            </div>
+        )
+    }
+
+    return (
+        <form className="space-y-8 divide-y divide-gray-200">
+            <div className="space-y-4">
+                <div className="space-y-4">
+                    <div className="space-y-2">
+                        <div>
+                            <label className="block text-sm font-medium text-gray-700">
+                                Colors
+                            </label>
+                            <div>
+                                <div id="adox-colors" className="flex">
+                                    <input
+                                        type="color"
+                                        onChange={(e) => onColorChanged(0, e.target.value)}
+                                        value={adoxData.colors[0]}
+                                        className='w-16px h-20px border-none p-0'
+                                    />
+                                    <input
+                                        type="color"
+                                        onChange={(e) => onColorChanged(1, e.target.value)}
+                                        value={adoxData.colors[1]}
+                                        className='w-16px h-20px border-none p-0'
+                                    />
+                                    <input
+                                        type="color"
+                                        onChange={(e) => onColorChanged(2, e.target.value)}
+                                        value={adoxData.colors[2]}
+                                        className='w-16px h-20px border-none p-0'
+                                    />
+                                    <input
+                                        type="color"
+                                        onChange={(e) => onColorChanged(3, e.target.value)}
+                                        value={adoxData.colors[3]}
+                                        className='w-16px h-20px border-none p-0'
+                                    />
+                                    <input
+                                        type="color"
+                                        onChange={(e) => onColorChanged(4, e.target.value)}
+                                        value={adoxData.colors[4]}
+                                        className='w-16px h-20px border-none p-0'
+                                    />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div >
+                            <label htmlFor="queryId" className="block text-sm font-medium text-gray-700">
+                                Query ID
+                            </label>
+                            <div className="mt-1">
+                                <input
+                                    type="text"
+                                    name="queryId"
+                                    id="queryId"
+                                    autoComplete="email"
+                                    value={adoxData.queryId}
+                                    onChange={(e) => { onQueryChanged(e.target.value) }}
+                                    className="block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 text-xs"
+                                />
+                            </div>
+                        </div>
+
+                        <div >
+                            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                                Email
+                            </label>
+                            <div className="mt-1">
+                                <input
+                                    type="text"
+                                    name="email"
+                                    id="email"
+                                    autoComplete="email"
+                                    value={adoxData.email}
+                                    onChange={(e) => { onEmailChanged(e.target.value) }}
+                                    className="block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 text-xs"
+                                />
+                            </div>
+                        </div>
+
+                        <div >
+                            <label htmlFor="organization" className="block text-sm font-medium text-gray-700">
+                                Organization
+                            </label>
+                            <div className="mt-1">
+                                <input
+                                    type="text"
+                                    name="organization"
+                                    id="organization"
+                                    value={adoxData.organization}
+                                    onChange={(e) => { onOrganizationChanged(e.target.value) }}
+                                    className="block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 text-xs"
+                                />
+                            </div>
+                        </div>
+
+                        <div >
+                            <label htmlFor="project" className="block text-sm font-medium text-gray-700">
+                                Project
+                            </label>
+                            <div className="mt-1">
+                                <input
+                                    type="text"
+                                    name="project"
+                                    id="project"
+                                    value={adoxData.project}
+                                    onChange={(e) => { onProjectChanged(e.target.value) }}
+                                    className="block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 text-xs"
+                                />
+                            </div>
+                        </div>
+
+                        <div >
+                            <label htmlFor="team" className="block text-sm font-medium text-gray-700">
+                                Team
+                            </label>
+                            <div className="mt-1">
+                                <input
+                                    type="text"
+                                    name="team"
+                                    id="team"
+                                    value={adoxData.team}
+                                    onChange={(e) => { onTeamChanged(e.target.value) }}
+                                    className="block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 text-xs"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className="pt-5">
+                    <div className="flex justify-end">
+                        <button
+                            type="button"
+                            className="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                            onClick={() => resetConfig()}
+                        >
+                            Reset
+                        </button>
+                        <button
+                            type="submit"
+                            className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                            onClick={() => saveConfig().then(() => window.close())}
+                        >
+                            Save
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    );
 }
 
 export default Highlight
