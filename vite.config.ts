2113bc12ad7e3a3b9e8b7c47c3a827168eaa9f7d
Alex Casasola <alex@casasola.dev>
2023-02-20 11:21:06 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.json'
