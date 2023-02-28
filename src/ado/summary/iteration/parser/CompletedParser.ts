f23122e25f61f23ac29f831a28022fd54c8c3e6a
Alex Casasola <alex@casasola.dev>
2023-02-28 11:42:18 -0800

    Update CompletedParser.ts

--- src/ado/summary/iteration/parser/CompletedParser.ts
+++ src/ado/summary/iteration/parser/CompletedParser.ts
@@ -10,7 +10,8 @@ export const CompletedParser: IterationItemParser = async (config, _workItem, wo
         if (
             (
                 historyEvent.fields?.["System.State"]?.newValue === 'Completed' ||
-                historyEvent.fields?.["System.State"]?.newValue === 'Resolved'
+                historyEvent.fields?.["System.State"]?.newValue === 'Resolved' ||
+                historyEvent.fields?.["System.State"]?.newValue === 'Closed'
             )
             ) {
                 completedTag.completedByMe = true
